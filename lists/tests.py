4e993389c0e72dd899c7cbc771eebe8b177063ba
krzisch <pekrzisch.pek@gmail.com>
2021-05-12 21:08:00 +0200

    Estending user story to deal with a second list item

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,5 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -11,9 +9,10 @@ class HomePageTest(TestCase):
 		self.assertEquals(found.func, home_page)
 
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf-8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
